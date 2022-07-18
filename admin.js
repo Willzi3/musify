95b805d201a0adcbeccd612acd179f2ef4f7659a
Willzi3 <78031861+Willzi3@users.noreply.github.com>
2022-07-18 23:14:46 +0200

    Add files via upload

--- admin.js
+++ admin.js
@@ -1,174 +1,254 @@
-let properties = JSON.parse(localStorage.getItem("Object"))
-? JSON.parse(localStorage.getItem("Object")):[
-    {
-        id: 1,
-        image: "https://picsum.photos/200/?random=1",
-        catergory: "Apartment",
-        location: "Melkbosstrand",
-        price: "2 500 000",
-        bathroom: '4'
+let houses = JSON.parse(localStorage.getItem("houses"))
+? JSON.parse(localStorage.getItem("houses")):[
+    {id:1,
+        image: 'https://picsum.photos/id/1008/200/300',
+     title:'Light and Modern Apartment',
+     price: '4500',
+     address:'2436 SW 8th St, Los Angeles, FL 33135, USA',
+     bedrooms:'3',
+     bathrooms:'2',
+     garage:'1',
+     size:'1200',
+     type:'Apartment',
+     user:' Samuel Palmer',
+     datePosted:'04/04/2020',
+     yearBuilt:'2016',
     },
-    {
-        id: 2,
-        image: "https://picsum.photos/200/?random=2",
-        catergory: "House",
-        location: "Clifton",
-        price: "4 000 000",
-        bathroom: '4'
-    },
-    {
-        id: 3,
-        image: "https://picsum.photos/200/?random=3",
-        catergory: "Studio",
-        location: "Table View",
-        price: "6 500 000",
-        bathroom: '3'
-    },
-    {
-        id: 4,
-        image: "https://picsum.photos/200/?random=4",
-        catergory: "Apartment",
-        location: "Llandudno",
-        price: "4 000 000",
-        bathroom: '2'
-    },
-    {
-        id: 5,
-        image: "https://picsum.photos/200/?random=5",
-        catergory: "House",
-        location: "Clifton",
-        price: "8 000 000",
-        bathroom: '1'
-    },
-    {
-        id: 6,
-        image: "https://picsum.photos/200/?random=6",
-        catergory: "Studio",
-        location: "Llandudno",
-        price: "4 000 000",
-        bathroom: '1'
-    },
-    {
-        id: 7,
-        image: "https://picsum.photos/200/?random=7",
-        catergory: "Apartment",
-        location: "Table View",
-        price: "2 500 000",
-        bathroom: '2'
-    },
-    {
-        id: 8,
-        image: "https://picsum.photos/200/?random=8",
-        catergory: "House",
-        location: "Clifton",
-        price: "8 000 000",
-        bathroom: '3'
-    },
-    {
-        id: 9,
-        image: "https://picsum.photos/200/?random=9",
-        catergory: "Studio",
-        location: "Melkbosstrand",
-        price: "2 500 000",
-        bathroom: '4'
-    },
-    {
-        id: 10,
-        image: "https://picsum.photos/200/?random=10",
-        catergory: "Apartment",
-        location: "Llandudno",
-        price: "6 500 000",
-        bathroom: '3'
-    },];
-//form for filtering and searchbar
-let formOptions = document.getElementById("form").innerHTML = `
 
-<form>
-<label>catergory</label>
-<select id="catergory" onchange="catergorie()">
-<option value="All">All</option>
-<option value="Apartment">Apartment</option>
-<option value="House">House</option>
-<option value="Studio">Studio</option>
-</select>
+    {id:2,
+        image:'https://picsum.photos/id/164/200/300',
+    title:'Cute Apartment',
+    price: '4500',
+    address:'2436 SW 8th St, Los Angeles, FL 33135, USA',
+    bedrooms:'3',
+    bathrooms:'2',
+    garage:'1',
+    size:'1200',
+    type:'Apartment',
+    user:' Samuel Palmer',
+    datePosted:'04/04/2020',
+    yearBuilt:'2016',
+   },
+   {id:3,
+    image: 'https://picsum.photos/id/1062/200/300',
+   title:'Cozy Apartment',
+   price: '4500',
+   address:'2436 SW 8th St, Miami, FL 33135, USA',
+   bedrooms:'2',
+   bathrooms:'2',
+   garage:'1',
+   size:'1200',
+   type:'Apartment',
+   user:' Samuel Palmer',
+   datePosted:'04/04/2020',
+   yearBuilt:'2016',
+  },
+  {id:4,
+    image: 'https://picsum.photos/id/238/200/300',
+  title:'Newly Built Apartment',
+  price: '4500',
+  address:'2436 SW 8th St, Chicago, FL 33135, USA',
+  bedrooms:'3',
+  bathrooms:'2',
+  garage:'1',
+  size:'1200',
+  type:'Apartment',
+  user:' Samuel Palmer',
+  datePosted:'04/04/2020',
+  yearBuilt:'2016',
+ },
+ {id:5,
+    image: 'https://picsum.photos/id/221/200/300',
+ title:'Fully Furnished Apartment',
+ price: '6000',
+ address:'2436 SW 8th St, Los Angeles, FL 33135, USA',
+ bedrooms:'2',
+ bathrooms:'4',
+ garage:'1',
+ size:'1200',
+ type:'Apartment',
+ user:' Samuel Palmer',
+ datePosted:'04/04/2020',
+ yearBuilt:'2016',
+},
+{id:6,
+    image: 'https://picsum.photos/id/556/200/300',
+     title:'North Facing House',
+     price: '6000',
+     address:'2436 SW 8th St, Miami, FL 33135, USA',
+     bedrooms:'5',
+     bathrooms:'2',
+     garage:'1',
+     size:'1200',
+     type:'House',
+     user:' Samuel Palmer',
+     datePosted:'04/04/2020',
+     yearBuilt:'2016',
+    },
+    {id:7,
+        image: 'https://picsum.photos/id/594/200/300',
+    title:'Newly Built House',
+    price: '4500',
+    address:'2436 SW 8th St, New York, FL 33135, USA',
+    bedrooms:'4',
+    bathrooms:'2',
+    garage:'1',
+    size:'1200',
+    type:'House',
+    user:' Samuel Palmer',
+    datePosted:'04/04/2020',
+    yearBuilt:'2016',
+   },
+   {id:8,
+    image: 'https://picsum.photos/id/142/200/300',
+   title:'Grand Mansion',
+   price: '10000',
+   address:'2436 SW 8th St, Miami, FL 33135, USA',
+   bedrooms:'5',
+   bathrooms:'2',
+   garage:'1',
+   size:'1200',
+   type:'House',
+   user:' Samuel Palmer',
+   datePosted:'04/04/2020',
+   yearBuilt:'2016',
+  },
+  {id:9,
+    image: 'https://picsum.photos/id/604/200/300',
+  title:'Bachelor Pad',
+  price: '4500',
+  address:'2436 SW 8th St, Chicago, FL 33135, USA',
+  bedrooms:'1',
+  bathrooms:'2',
+  garage:'1',
+  size:'1200',
+  type:'Studio',
+  user:' Samuel Palmer',
+  datePosted:'04/04/2020',
+  yearBuilt:'2016',
+ },
+ {id:10,
+    image: 'https://picsum.photos/id/1031/200/300',
+ title:'Penthouse Open Floor',
+ price: '10000',
+ address:"'2436 SW 8th St',' New York','FL 33135 USA'",
+ bedrooms:'2',
+ bathrooms:'2',
+ garage:'1',
+ size:'1200',
+ type:'Studio',
+ user:' Samuel Palmer',
+ datePosted:'04/04/2020',
+ yearBuilt:'2016',
+},];
 
-
-<label>price</label>
-<select  id="price" onchange="prices()">
-<option value="All">All</option>
-<option value="2 500 000">R2 500 000</option>
-<option value="4 000 000">R4 000 000</option>
-<option value="6 500 000">R6 500 000</option>
-<option value="8 000 000">R8 000 000</option>
-</select>
-
-
-<label>Bathroom</label>
-<select onchange="bathrooms()" id="bathroom">
-<option value="All">All</option>
-<option value="1">1</option>
-<option value="2">2</option>
-<option value="3">3</option>
-<option value="4">4</option>
-</select>
-
-
-<label>location</label>
-<select onchange="locations()" id="location">
-<option value="All">All</option>
-<option value="Melkbosstrand">Melkbosstrand</option>
-<option value="Table View">Table View</option>
-<option value="Clifton">Clifton</option>
-<option value="Llandudno">Llandudno</option>
-</select>
-</form>
-`
-localStorage.setItem("Object", JSON.stringify(properties));
+localStorage.setItem("houses", JSON.stringify(houses));
 // display local storage    
 // document.getElementById("propertyList").innerHTML = JSON.parse( localStorage.getItem("houses"));
-   
+    const propertiesTable = document.querySelector("#propertylist");
 
-let search = document.getElementById("Output").innerHTML = `
-<div class="search-container">
-<input type="text" name="search" id="searchBar" placeholder="Search..."/>
-<button type="button" onclick="Search()">Search</button>
-</div>
-`
+function ShowHouseListings (houses) {
+    document.querySelector("#propertylist").innerHTML = "";
+  
+    houses.forEach(house => {
+      propertiesTable.innerHTML += `
+        <tr>
+        <td>${house.id}</td>
+          <td><img src=${house.image}></td>
+          <td>${house.type}</td>
+          <td>${house.bathrooms}</td>
+          <td>${house.price}</td>
+          <td><button onclick="editHouse(${house.id})">✎</button> </td><td><button onclick="deleteHouse(${house.id})">✂</button></td>
+        <tr>
+      `;
+    })
+  };
+  
+ShowHouseListings(houses);
+
+// delete function
+function deleteHouse(id) {
+  houses = houses.filter((house) => {
+    return house.id !== id;     //loop through houses and return houses which id noes not match
+  });   
+  localStorage.setItem("houses", JSON.stringify(houses));
+  ShowHouseListings(houses);
+}
 
 
+// edit house listing 
+function editHouse(id) {
+  let house = houses.find((house) => house.id === id); //Finds the id of button that was clicked
+  let input = prompt("Input Changes"); //Allows the user to input their own changes
+  house.title = input; //Changes the content to what the user's input
+  // addToStorage(objectives); //Adds to local storage
+  ShowHouseListings(houses);
+}
 
-function Search() {
-    let search = document.getElementById("searchBar").value;
-    for(i = 0; i < properties.length; i++){
-        if(search === properties[i].catergory){
-            document.getElementById("Output").innerHTML += `
-            <div class="searchy">
-            <div><img src="${properties[i].image}"/></div>
-            <div>${properties[i].price}</div>
-            <div>${properties[i].catergory}</div>
-            <div>${properties[i].location}</div>
-            </div>`
-        } 
-        else if (search === properties[i].location){
-            document.getElementById("Output").innerHTML += `
-            <div class="searchy">
-            <div class="search-item"><img src="${properties[i].image}"/></div>
-            <div class="search-item">${properties[i].price}</div>
-            <div class="search-item">${properties[i].catergory}</div>
-            <div class="search-item">${properties[i].location}</div>
-            <button type="button" onclick="delete()">delete</button>
-            <button type="button" onclick="edit()">edit</button>
-            </div>`
-        }
-       
-    }
-    }
+function addListing() {
+  let listing = {
+    id: houses.length + 1,
 
-    // delete function
-function deleteProperties(id) {
-    Properties = Properties.filter((property) => {
-      return property.id !== id;     //loop through houses and return houses which id noes not match
-    });   
-    localStorage.setItem("Object", JSON.stringify(Properties));
-    
+    image: document.querySelector("#Image").value,
+    type: document.querySelector("#Type").value,
+    baths: document.querySelector("#Bathrooms").value,
+    price: document.querySelector("#Price").value,
+  };
+  houses.push(listing);
+  localStorage.setItem("properties", JSON.stringify(houses));
+  
+  ShowHouseListings(houses);
+  console.log(houses);
+}
+
+function typeHouse() {
+  let value = document.getElementById("comboA").value;
+  //display entire array when all is selected
+  if (value === "All") {
+    return ShowHouseListings(houses);
   }
+  //create array to show only desired housesS
+  let filteredHouses = houses.filter((house) => {
+    return house.type === value     //loop through houses and return houses which id noes not match
+  });   
+  ShowHouseListings(filteredHouses);
+}
+
+function addressHouse() {
+  let value = document.getElementById("comboB").value;
+  //display entire array when all is selected
+  if (value === "All") {
+    return ShowHouseListings(houses);
+  }
+  let filteredHouses = houses.filter((house) => {
+    // create condition to search for value inside of the addrress
+    if(house.address.includes(value)){ 
+    return house.address } 
+        //loop through houses and return houses which id noes not match
+  });   
+  ShowHouseListings(filteredHouses);}
+
+function bedroomsHouse() {
+  let value = document.getElementById("comboC").value;
+  //display entire array when all is selected
+  if (value === "All") {
+    return ShowHouseListings(houses);
+  }
+  //create array to show only desired housesS
+  let filteredHouses = houses.filter((house) => {
+    return house.bedrooms === value     //loop through houses and return houses which id noes not match
+  });   
+  ShowHouseListings(filteredHouses);}
+
+function priceHouse() {
+  let value = document.getElementById("comboD").value;
+  //display entire array when all is selected
+  if (value === "All") {
+    return ShowHouseListings(houses);
+  }
+  //create array to show only desired houses
+  let filteredHouses = houses.filter((house) => {
+    return house.price === value     //loop through houses and return houses which id noes not match
+  });   
+  ShowHouseListings(filteredHouses);}
+
--- script.js
+++ script.js
@@ -1,215 +1,224 @@
-properties = [
-    {
-        id: 1,
-        image: "https://picsum.photos/200/?random=1",
-        catergory: "Apartment",
-        location: "Melkbosstrand",
-        price: "2 500 000",
-        bathroom: '4'
+let houses = JSON.parse(localStorage.getItem("houses"))
+? JSON.parse(localStorage.getItem("houses")):[
+    {id:1,
+        image: 'https://picsum.photos/id/1008/200/300',
+     title:'Light and Modern Apartment',
+     price: '4500',
+     bedrooms:'3',
+     bathrooms:'2',
+     garage:'1',
+     size:'1200',
+     type:'Apartment',
+     user:' Samuel Palmer',
+     datePosted:'04/04/2020',
+     yearBuilt:'2016',
     },
-    {
-        id: 2,
-        image: "https://picsum.photos/200/?random=2",
-        catergory: "House",
-        location: "Clifton",
-        price: "4 000 000",
-        bathroom: '4'
+
+    {id:2,
+        image:'https://picsum.photos/id/164/200/300',
+    title:'Cute Apartment',
+    price: '4500',
+    bedrooms:'3',
+    bathrooms:'2',
+    garage:'1',
+    size:'1200',
+    type:'Apartment',
+    user:' Samuel Palmer',
+    datePosted:'04/04/2020',
+    yearBuilt:'2016',
+   },
+   {id:3,
+    image: 'https://picsum.photos/id/1062/200/300',
+   title:'Cozy Apartment',
+   price: '4500',
+   bedrooms:'2',
+   bathrooms:'2',
+   garage:'1',
+   size:'1200',
+   type:'Apartment',
+   user:' Samuel Palmer',
+   datePosted:'04/04/2020',
+   yearBuilt:'2016',
+  },
+  {id:4,
+    image: 'https://picsum.photos/id/238/200/300',
+  title:'Newly Built Apartment',
+  price: '4500',
+  bedrooms:'3',
+  bathrooms:'2',
+  garage:'1',
+  size:'1200',
+  type:'Apartment',
+  user:' Samuel Palmer',
+  datePosted:'04/04/2020',
+  yearBuilt:'2016',
+ },
+ {id:5,
+    image: 'https://picsum.photos/id/221/200/300',
+ title:'Fully Furnished Apartment',
+ price: '6000',
+ bedrooms:'2',
+ bathrooms:'4',
+ garage:'1',
+ size:'1200',
+ type:'Apartment',
+ user:' Samuel Palmer',
+ datePosted:'04/04/2020',
+ yearBuilt:'2016',
+},
+{id:6,
+    image: 'https://picsum.photos/id/556/200/300',
+     title:'North Facing House',
+     price: '6000',
+     bedrooms:'5',
+     bathrooms:'2',
+     garage:'1',
+     size:'1200',
+     type:'House',
+     user:' Samuel Palmer',
+     datePosted:'04/04/2020',
+     yearBuilt:'2016',
     },
-    {
-        id: 3,
-        image: "https://picsum.photos/200/?random=3",
-        catergory: "Studio",
-        location: "Table View",
-        price: "6 500 000",
-        bathroom: '3'
-    },
-    {
-        id: 4,
-        image: "https://picsum.photos/200/?random=4",
-        catergory: "Apartment",
-        location: "Llandudno",
-        price: "4 000 000",
-        bathroom: '2'
-    },
-    {
-        id: 5,
-        image: "https://picsum.photos/200/?random=5",
-        catergory: "House",
-        location: "Clifton",
-        price: "8 000 000",
-        bathroom: '1'
-    },
-    {
-        id: 6,
-        image: "https://picsum.photos/200/?random=6",
-        catergory: "Studio",
-        location: "Llandudno",
-        price: "4 000 000",
-        bathroom: '1'
-    },
-    {
-        id: 7,
-        image: "https://picsum.photos/200/?random=7",
-        catergory: "Apartment",
-        location: "Table View",
-        price: "2 500 000",
-        bathroom: '2'
-    },
-    {
-        id: 8,
-        image: "https://picsum.photos/200/?random=8",
-        catergory: "House",
-        location: "Clifton",
-        price: "8 000 000",
-        bathroom: '3'
-    },
-    {
-        id: 9,
-        image: "https://picsum.photos/200/?random=9",
-        catergory: "Studio",
-        location: "Melkbosstrand",
-        price: "2 500 000",
-        bathroom: '4'
-    },
-    {
-        id: 10,
-        image: "https://picsum.photos/200/?random=10",
-        catergory: "Apartment",
-        location: "Llandudno",
-        price: "6 500 000",
-        bathroom: '3'
-    }
-]
-//Localstorage
-let myObject = JSON.stringify(properties);
+    {id:7,
+        image: 'https://picsum.photos/id/594/200/300',
+    title:'Newly Built House',
+    price: '4500',
+    bedrooms:'4',
+    bathrooms:'2',
+    garage:'1',
+    size:'1200',
+    type:'House',
+    user:' Samuel Palmer',
+    datePosted:'04/04/2020',
+    yearBuilt:'2016',
+   },
+   {id:8,
+    image: 'https://picsum.photos/id/142/200/300',
+   title:'Grand Mansion',
+   price: '10000',
+   bedrooms:'5',
+   bathrooms:'2',
+   garage:'1',
+   size:'1200',
+   type:'House',
+   user:' Samuel Palmer',
+   datePosted:'04/04/2020',
+   yearBuilt:'2016',
+  },
+  {id:9,
+    image: 'https://picsum.photos/id/604/200/300',
+  title:'Bachelor Pad',
+  price: '4500',
+  bedrooms:'1',
+  bathrooms:'2',
+  garage:'1',
+  size:'1200',
+  type:'Studio',
+  user:' Samuel Palmer',
+  datePosted:'04/04/2020',
+  yearBuilt:'2016',
+ },
+ {id:10,
+    image: 'https://picsum.photos/id/1031/200/300',
+ title:'Penthouse Open Floor',
+ price: '10000',
+ bedrooms:'2',
+ bathrooms:'2',
+ garage:'1',
+ size:'1200',
+ type:'Studio',
+ user:' Samuel Palmer',
+ datePosted:'04/04/2020',
+ yearBuilt:'2016',
+},];
 
-localStorage.setItem("Object", myObject);
-let object = JSON.parse(localStorage.getItem("Object"));
-console.log(properties);
+   const propertiesContainer = document.querySelector("#properties");
+
+// add array to local storage   
+   localStorage.setItem("houses", JSON.stringify(houses));
+   
+// display local storage    
+document.getElementById("properties").innerHTML = JSON.parse( localStorage.getItem("houses"));
+
+function showHouses (houses) {
+    propertiesContainer.innerHTML = "";
+    houses.forEach(house => {
+        propertiesContainer.innerHTML += `
+            <div id='housecards'>
+                <img src=${ house.image } />
+                 <h3>${house.title}</h3>
 
 
-//form for filtering and searchbar
-let formOptions = document.getElementById("form").innerHTML = `
-<form>
-<label>catergory</label>
-<select id="catergory" onchange="catergorie()">
-<option value="All">All</option>
-<option value="Apartment">Apartment</option>
-<option value="House">House</option>
-<option value="Studio">Studio</option>
-</select>
+                 <ul id=house-values>
+                 <li><i class="fa-solid fa-bed"></i>${house.bedrooms}</li>
+                 <li><i class="fa-solid fa-shower"></i>${house.bathrooms}</li>
+                 <li><i class="fa-solid fa-dollar"></i>${house.price}/mo</li>
+                <li><i class="fa-solid fa-car"></i>${house.garage}</li>
+                <li><i class="fa-solid fa-ruler"></i>${house.size}</li>
+                
 
 
-<label>price</label>
-<select  id="price" onchange="prices()">
-<option value="All">All</option>
-<option value="2 500 000">R2 500 000</option>
-<option value="4 000 000">R4 000 000</option>
-<option value="6 500 000">R6 500 000</option>
-<option value="8 000 000">R8 000 000</option>
-</select>
-
-
-<label>Bathroom</label>
-<select onchange="bathrooms()" id="bathroom">
-<option value="All">All</option>
-<option value="1">1</option>
-<option value="2">2</option>
-<option value="3">3</option>
-<option value="4">4</option>
-</select>
-
-
-<label>location</label>
-<select onchange="locations()" id="location">
-<option value="All">All</option>
-<option value="Melkbosstrand">Melkbosstrand</option>
-<option value="Table View">Table View</option>
-<option value="Clifton">Clifton</option>
-<option value="Llandudno">Llandudno</option>
-</select>
-</form>
-`
-function showProperties (properties) {
-    for(i = 0; i < properties.length; i++){
-        console.log(
-            document.getElementById("output").innerHTML += `
-            <div class="card-container">
-            <div class="card">
-            <h2>${properties[i].catergory}</h2>
-            <p>${properties[i].location}</p>
-            <p>${properties[i].price}</p>
-            <img src="${properties[i].image}">
             </div>
-            </div>`
-        );
+        `
+    })
+};
+
+showHouses(houses);
+
+function typeHouse() {
+    let value = document.getElementById("comboA").value;
+    if (value === "All") {
+      return showHouses(houses);
     }
+    let filteredHouses = houses.filter((house) => {
+      return house.type === value 
+    });   
+    showHouses(filteredHouses);
+  }
+
+  function addressHouse() {
+    let value = document.getElementById("comboB").value;
+    if (value === "All") {
+      return showHouses(houses);
+    }
+    let filteredHouses = houses.filter((house) => {
+      if(house.address.includes(value)){ 
+      return house.address } 
+    });   
+    showHouses(filteredHouses);
+  }
+
+  function bedroomsHouse() {
+    let value = document.getElementById("comboC").value;
+    if (value === "All") {
+      return showHouses(houses);
+    }
+    let filteredHouses = houses.filter((house) => {
+      return house.bedrooms === value
+    });   
+    showHouses(filteredHouses);
+  }
+
+  function priceHouse() {
+    let value = document.getElementById("comboD").value;
+    if (value === "All") {
+      return showHouses(houses);
+    }
+
+
+    let filteredHouses = houses.filter((house) => {
+      return house.price === value 
+    });   
+    showHouses(filteredHouses);
+  }
+
+function sortHouses(){
+    let price = houses.sort((a, b) => {
+      return a.price - b.price;
+    });
+    let retrievedList = price;
+    document.querySelector("#properties").innerHTML = "";
+    retrievedList.forEach((propperty) => {
+    showHouses(retrievedList)
+  })
 }
-
-
-showProperties(properties);
-
-function catergorie() {
-    let value = document.getElementById("catergory").value;
-    //display entire array when all is selected
-    if (value === "All") {
-      return showProperties(properties);
-    }
-    //create array to show only desired housesS
-    let filteredProperties = properties.filter((property) => {
-      return property.catergory === value     //loop through houses and return houses which id noes not match
-    });   
-    showProperties(filteredProperties);
-  }
-
-  function locations() {
-    let value = document.getElementById("location").value;
-    //display entire array when all is selected
-    if (value === "All") {
-      return showProperties(properties);
-    }
-    let filteredProperties = properties.filter((property) => {
-      // create condition to search for value inside of the addrress
-      if(property.location.includes(value)){ 
-      return property.location } 
-          //loop through houses and return houses which id noes not match
-    });   
-    showProperties(filteredProperties);
-  }
-
-  function bathrooms() {
-    let value = document.getElementById("bathroom").value;
-    //display entire array when all is selected
-    if (value === "All") {
-      return showProperties(properties);
-    }
-    //create array to show only desired housesS
-    let filteredProperties = properties.filter((property) => {
-      return property.bathroom === value     //loop through houses and return houses which id noes not match
-    });   
-    showProperties(filteredProperties);
-  }
-
-  function prices() {
-    let value = document.getElementById("price").value;
-    //display entire array when all is selected
-    if (value === "All") {
-      return showProperties(properties);
-    }
-    //create array to show only desired houses
-    let filteredProperties = properties.filter((property) => {
-      return property.price === value     //loop through houses and return houses which id noes not match
-    });   
-    showProperties(filteredProperties);
-  }
-
-// function sortHouses(){
-//     let price = houses.sort((a, b) => {
-//       return a.price - b.price;
-//     });
-//     let retrievedList = price;
-//     document.querySelector("#properties").innerHTML = "";
-//     retrievedList.forEach((property) => {
-//     showProperties(retrievedList)
-//   })
-// }
